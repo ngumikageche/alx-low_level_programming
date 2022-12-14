d54dd78daf7e14d50b4a44007b057296393b1b1d
Kageche <ngumi98@gmail.com>
2022-12-13 21:57:40 -0800

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,15 @@
 #include"main.h"
 /**
- *print_alphabet- prints letters from a-z using a loop and ascii characters
+ *print_alphabet_x10- prints letters from a-z using a loop and ascii characters
  */
 void print_alphabet_x10(void)
 {
 char letter;
 int count;
-/**
- *prints the aphabets 10 times
- */
-for(count = 0; count<=9; count++)
+/* prints the aphabets 10 times */
+for(count = 0; count <= 9; count++)
 {     
-/**
- *first loop to print the letters
- */
+/* first loop to print the letters */
 for (letter = 97; letter <= 122; letter++)
 {
 _putchar(letter);
