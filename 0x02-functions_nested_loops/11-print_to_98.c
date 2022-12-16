a1a4ff3416fb670a4ab52a25ee68894cf210e9d0
Kageche <ngumi98@gmail.com>
2022-12-16 05:36:15 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ *print_to_98- function that prints all natural numbers from n to 98
+ *@n: number (passed number by function)
+ */
+void print_to_98(int n)
+{
+int count;
+if (n < 98)
+{
+for (count = n; count < 99; count++)
+{
+printf("%d", count);
+if (count == 98)
+{
+break;
+}
+_putchar(',');
+_putchar(' ');
+}
+}
+else
+{
+for (count = n; count >= 98; count--)
+{
+printf("%d", count);
+if (count == 98)
+{
+break;
+}
+_putchar(',');
+_putchar(' ');
+}
+}
+putchar('\n');
+}
