9ed24e22fafc502aac28dbacda559de1992d485b
Kageche <ngumi98@gmail.com>
2022-12-19 04:50:24 -0800

    I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include"main.h"
+#include<string.h>
+/**
+ *print_rev- function that returns the length of a string
+ *@s:string
+ */
+void print_rev(char *s)
+{
+int count;
+int stl = strlen(s);
+for (count = stl; count >= 0; count--)
+{
+_putchar(s[count]);
+}
+_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,4 +6,6 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 char _putchar(char ch);
+void print_rev(char *s);
+void rev_string(char *s);
 #endif
