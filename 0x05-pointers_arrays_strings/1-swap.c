4ca9c02c7207ae77a8cb9917c987e17e4226b591
Kageche <ngumi98@gmail.com>
2022-12-19 02:31:15 -0800

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+/**
+ *swap_int- function that swaps two ints
+ *@a:first int
+ *@b:Second int
+ */
+void swap_int(int *a, int *b)
+{
+int a2 = *a;
+*a = *b;
+*b = a2;
+}
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,10 @@
+#include<string.h>
+/**
+ *_strlen- function that returns the length of a string
+ *@s:string
+ *Return: length of s
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
