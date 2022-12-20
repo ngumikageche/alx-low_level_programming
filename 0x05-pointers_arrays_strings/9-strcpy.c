794a253e63c4216cb78074321c9ebe9348b8d1ec
Kageche <ngumi98@gmail.com>
2022-12-20 00:28:46 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include<string.h>
+/**
+ *_strcpy-a function that copies the string
+ *@src:pointer by
+ *@dest:pointer to destination
+ *Return: value :the pointer dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+return (strcpy(dest, src));
+}
