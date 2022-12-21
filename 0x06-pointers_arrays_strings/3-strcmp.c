92080eadad05106aa6bda864253b3a8f934e7518
Kageche <ngumi98@gmail.com>
2022-12-21 09:05:06 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include<string.h>
+/**
+ *_strcmp-function that compare two strings
+ *@s1: The first string to be compared
+ *@s2: the second string to be compared
+ *Return:if return value is less than 0 s1 is < s2, if v > 0 s2 < s1
+ */
+int _strcmp(char *s1, char *s2)
+{
+return (strcmp(s1, s2));
+}
