af4d13652fc441551fe84018139445f1682b64b9
Kageche <ngumi98@gmail.com>
2022-12-21 04:29:46 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include<string.h>
+/**
+ *_strcat-Function that concatenates two strings
+ *@dest:pointer to the destination array
+ *@src:This is the string to append
+ *Return: a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+return (strcat(dest, src));
+}
