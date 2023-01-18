f50d8933d212c679e38c3c47a76189be05adca83
Kageche <ngumi98@gmail.com>
2023-01-18 03:34:45 -0800

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+struct dog
+{
+  char *name;
+  float age;
+  char *owner;
+};
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
