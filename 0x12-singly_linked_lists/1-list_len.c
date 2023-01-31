d4e8554a6b03235439284d41bb19acc4d54999fe
Th3blacklist <ngumi98@gmail.com>
2023-01-31 10:40:41 +0300

    List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
  *list_len- counts the number of elements in a linked list_t list.
  *@h:first node
  *Return: the number of elements in a linked list_t list
- */x
+ */
 size_t list_len(const list_t *h)
 {
 int count = 0;
