6738845c958aae50bf1b4623fda664f7024c7bb8
Joseph Kageche <ngumi98@gmail.com>
2023-02-26 11:42:29 +0300

    Bit manipulation

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+unsigned int binary_to_uint(const char *b);
+char _putchar(char ch);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+#endif
